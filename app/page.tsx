ea9c38580fb89e6e19f0e1fb3d18cc7bf076a425
kanakanho <kahoshibakichi@gmail.com>
2024-04-30 20:14:12 +0900

    fix: 地面を非表示に

--- app/page.tsx
+++ app/page.tsx
@@ -22,14 +22,14 @@ const Home: React.FC = () => {
       >
         <orthographicCamera attach='shadow-camera' args={[-2500, 2500, 2500, -2500, 1, 5000]} />
       </directionalLight>
-      <Plane args={[1e5, 1e5]} position={[0, 12, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+      {/* <Plane args={[1e5, 1e5]} position={[0, 12, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
         <meshStandardMaterial color='white' />
-      </Plane>
+      </Plane> */}
       <PlateauTilesetTransform>
-        {/* <PlateauTileset path='bldg/13100_tokyo/13101_chiyoda-ku/notexture' center /> */}
-        {/* <PlateauTileset path='bldg/13100_tokyo/13102_chuo-ku/notexture' /> */}
-        <PlateauTileset path='bldg/23100_nagoya/23101_chikusa-ku/notexture'  />
-        <PlateauTileset path='bldg/23100_nagoya/23102_higashi-ku/notexture' center/>
+        <PlateauTileset path='bldg/13100_tokyo/13101_chiyoda-ku/notexture' center />
+        <PlateauTileset path='bldg/13100_tokyo/13102_chuo-ku/notexture' />
+        <PlateauTileset path='bldg/23100_nagoya/23101_chikusa-ku/notexture' />
+        <PlateauTileset path='bldg/23100_nagoya/23102_higashi-ku/notexture' center />
       </PlateauTilesetTransform>
       <EffectComposer>
         <SSAO intensity={5000} />
